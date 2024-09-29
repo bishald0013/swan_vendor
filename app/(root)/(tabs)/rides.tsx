662b80e4b89f9f27a8781c280ffcfb379da89a21
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2024-09-29 22:31:59 +0530

    first commit

---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const History = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>History</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default History;
